e8ad16190fd9a895b1769dc6351fdf8f7a4b8f08
HuzaifaBilal <huzaifa@pragtex.com>
2024-01-11 12:31:05 +0530

    lc- basic setup and Header.

---
+++ frontend/src/app/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { HeaderComponent } from './components/header/header.component';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, HeaderComponent],
+})
+export class HomePageModule {}
