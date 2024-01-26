6739b61870d9aa92c6ce4adf3ac6596d6decb888
HuzaifaBilal <huzaifabilal2191@gmail.com>
2024-01-26 22:43:28 +0530

    lc-feat header component

--- frontend/src/app/home/components/header/header.component.ts
+++ frontend/src/app/home/components/header/header.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from './popover/popover.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit {
+  constructor(public popoverController: PopoverController) {}
 
   ngOnInit() {}
+  async presentPopover(e: Event) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      event: e,
+    });
 
+    await popover.present();
+
+    const { role } = await popover.onDidDismiss();
+    console.log(`Popover dismissed with role: ${role}`);
+  }
 }
